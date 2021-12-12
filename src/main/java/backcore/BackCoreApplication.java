94a223333cfabaf53bcaf5a6daefed91f9dd845f
IvanVF <fedinivanvladimirovich@mail.ru>
2021-12-12 15:40:24 +0700

    Back-core initial commit

---
+++ src/main/java/backcore/BackCoreApplication.java
@@ -0,0 +1,13 @@
+package backcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCoreApplication.class, args);
+	}
+
+}
